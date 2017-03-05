0e0dd2b42b31b3ff3d30b0334144a0a12171a7a0
desinole <desi.american@gmail.com>
2017-03-05 00:13:05 -0500

    Added api calls for sessions and sponsors

---
+++ codecampster/Controllers/SpeakerListController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using codecampster.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace codecampster.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SpeakerList")]
+    public class SpeakerListController : Controller
+    {
+        private ApplicationDbContext _context;
+        public SpeakerListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/TimeslotList
+        [HttpGet]
+        [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any)]
+        public IEnumerable<Speaker> GetSpeakers()
+        {
+            return _context.Speakers.OrderBy(s=>s.FullName);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ codecampster/SponsorListController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using codecampster.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace codecampster
+{
+    [Produces("application/json")]
+    [Route("api/SponsorList")]
+    public class SponsorListController : Controller
+    {
+        private ApplicationDbContext _context;
+        public SponsorListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/TimeslotList
+        [HttpGet]
+        [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any)]
+        public IEnumerable<Sponsor> GetSponsors()
+        {
+            return _context.Sponsors.OrderBy(s => s.CompanyName);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
